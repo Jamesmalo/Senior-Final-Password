04c49065314f37a049e191227da09648913c64ce
Jamesmalo <9jmalo@stpatrick.org>
2017-05-03 11:25:04 -0500

    day 3 (almost done)

--- passwordGUI.js
+++ passwordGUI.js
@@ -10,10 +10,26 @@ function getString(){
 
     password.question("What would you like to set as your password?", function(str){
         let passesAllChecks = true;
-        
+
         //Conditional Statements here.
-
-
+        if(testString.inRange() == false){
+          return false;
+        }
+        if(testString.checkLength() == false){
+            return false;
+        }
+        if(testString.containsUpper() == false){
+          return false;
+        }
+        if(testString.containsLower() == false){
+          return false;
+        }
+        if(testString.containsNumerical() == false){
+          return false;
+        }
+        if(testString.containsSpecial() == false){
+          return false;
+        }
         if(!passesAllChecks){
             console.log("\nTry again\n");
             password.close();
